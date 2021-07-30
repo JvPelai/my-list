42d1e04b1d23e30bed4ea16f3a5786f6de861222
JvPelai <jv.pelai@gmail.com>
2021-07-30 13:16:41 -0300

    refat setup

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,21 +1,25 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
-import { User } from './entity/User';
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+dotenv.config();
+const port = process.env.PORT;
 
 createConnection()
-  .then(async (connection) => {
-    console.log('Inserting a new user into the database...');
-    const user = new User();
-    user.firstName = 'Timber';
-    user.lastName = 'Saw';
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log('Saved a new user with id: ' + user.id);
+  .then(async () => {
+    const app = express();
+    app.use(helmet());
+    app.use(cors());
+    app.use(express.json());
 
-    console.log('Loading users from the database...');
-    const users = await connection.manager.find(User);
-    console.log('Loaded users: ', users);
+    app.get('/', (req, res) => {
+      res.json({ message: 'Hello World!' });
+    });
 
-    console.log('Here you can setup and run express/koa/any other framework.');
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
   })
   .catch((error) => console.log(error));
