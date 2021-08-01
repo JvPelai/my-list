e74331eba6f6ad7ab563eb8b54ec437a0e90e950
JvPelai <jv.pelai@gmail.com>
2021-07-31 21:33:07 -0300

    user items relation

--- packages/server/src/services/TodoItemServices.ts
+++ packages/server/src/services/TodoItemServices.ts
@@ -2,15 +2,20 @@ import { JwtPayload, verify } from 'jsonwebtoken';
 import { getCustomRepository } from 'typeorm';
 import { TodoItemDTO } from '../dto/todo-item.dto';
 import { TodoItemsRepositories } from '../repositories/TodoItemRepository';
+import { UsersRepositories } from '../repositories/UserRepository';
 
 const createTodoItem = async (
   data: TodoItemDTO,
   token: string
 ): Promise<string | JwtPayload> => {
   const repository = getCustomRepository(TodoItemsRepositories);
+  const userRepository = getCustomRepository(UsersRepositories);
   const user = verify(token, 'supersecret');
-  const item = { ...data, user: user.sub };
-  const todoItem = repository.create(item);
+  const todoItem = repository.create(data);
+  const createdBy = await userRepository.findOne({ where: { id: user.sub } });
+  if (createdBy) {
+    todoItem.createdBy = createdBy;
+  }
   await repository.save(todoItem);
   return user;
 };
